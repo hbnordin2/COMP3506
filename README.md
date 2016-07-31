49989cabc204e161a0a4e5b6d5e30b2b1e0b1d16
Cameron Aavik <cameronaavik@gmail.com>
2016-07-31 19:54:11 +1000

    Added RAM Model Interpreter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # COMP3506
 Everyone's solutions for COMP3506
+
+To run the rammodel python script. Create a .ram file with the code you want to execute and call:
+
+`python rammodel.py example.ram`
---
+++ rammodel.py
@@ -0,0 +1,70 @@
+# Defined rules so far:
+# 1. Register Value Set
+#     R1 <- 2
+#     R31 <- 2132
+
+import sys
+
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def is_register(s):
+    if s.startswith("R"):
+        reg_number = s.split("R")[1]
+    elif s.startswith("r"):
+        reg_number = s.split("r")[1]
+    else:
+        return False
+    return is_int(reg_number)
+
+
+with open(sys.argv[1]) as f:
+    # load first line into memory (ignoring comments)
+    lines = [l.split("//")[0] for l in f.read().splitlines()]
+
+# get the initial memory from the first line
+memory = [int(x) & 0xffffffff for x in lines[0].strip().split()]
+# remove the first line which is no longer needed and remove all comments
+code = lines[1:]
+
+# this will store any branches to return to
+branches = {}
+
+for line_num in range(len(code)):
+    c = code[line_num]
+    # if it ends in a colon, then it is a branch point
+    if c.endswith(":"):
+        branches[c.split(":")[0].strip()] = line_num
+
+program_counter = 0
+registers = [0] * 32
+running_time = 0
+
+while program_counter < len(code):
+    line = code[program_counter]
+    arrow_split = line.split("<-")
+    if len(arrow_split) == 2:
+        lhs = arrow_split[0].strip()
+        rhs = arrow_split[1].strip()
+        # if the left hand side starts with R or r, and the right hand side is a number.
+        # then this is an instruction for rule one.
+        if is_register(lhs) and is_int(rhs):
+            if lhs.startswith("R"):
+                register_number = int(lhs.split("R")[1])
+            elif lhs.startswith("r"):
+                register_number = int(lhs.split("r")[1])
+            value = int(rhs)
+            if 0 < register_number < 32:
+                registers[register_number] = value
+                running_time += 1
+    program_counter += 1
+
+print("Register Values: " + ' '.join(str(n) for n in registers))
+print("Memory Values: " + ' '.join(str(n) for n in memory))
+print("Running Time: " + str(running_time))
